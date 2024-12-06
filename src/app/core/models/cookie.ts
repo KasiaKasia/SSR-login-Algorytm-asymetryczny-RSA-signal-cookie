c261f4049cf0ceaa13fdea115f87a196d79211c8
--add <bulicka.alexandra@gmail.com>
2024-12-06 01:51:12 +0100

    [refactor] - refactor

--- src/app/core/models/cookie.ts
+++
@@ -1,5 +0,0 @@
-export interface Cookie {
-    getCookie(name: string): string | null;
-    setCookie(name: string, value: string, options: { [key: string]: any }): void;
-    deleteCookie(name: string): void;
-}
